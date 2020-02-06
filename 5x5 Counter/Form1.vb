23f4b897cbaf028beb717385fbe408653c90e7f9
Your Name <you@example.com>
2020-02-06 13:41:34 +0800

    Secong Commit

--- 5x5 Counter/Form1.vb
+++ 5x5 Counter/Form1.vb
@@ -1,3 +1,301 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Timer1.Start()
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        TimerLabel.Text += 1
+
+        If TimerLabel.Text = 1 Then
+            Label1.BackColor = Color.LightBlue
+            Label2.BackColor = Color.Firebrick
+            Label3.BackColor = Color.Firebrick
+            Label4.BackColor = Color.LightBlue
+            Label5.BackColor = Color.LightBlue
+            Label6.BackColor = Color.LightBlue
+            Label7.BackColor = Color.LightBlue
+            Label8.BackColor = Color.Firebrick
+            Label9.BackColor = Color.LightBlue
+            Label10.BackColor = Color.LightBlue
+            Label11.BackColor = Color.LightBlue
+            Label12.BackColor = Color.LightBlue
+            Label13.BackColor = Color.Firebrick
+            Label14.BackColor = Color.LightBlue
+            Label15.BackColor = Color.LightBlue
+            Label16.BackColor = Color.LightBlue
+            Label17.BackColor = Color.LightBlue
+            Label18.BackColor = Color.Firebrick
+            Label19.BackColor = Color.LightBlue
+            Label20.BackColor = Color.LightBlue
+            Label21.BackColor = Color.LightBlue
+            Label22.BackColor = Color.Firebrick
+            Label23.BackColor = Color.Firebrick
+            Label24.BackColor = Color.Firebrick
+            Label25.BackColor = Color.LightBlue
+
+        ElseIf TimerLabel.Text = 2 Then
+            Label1.BackColor = Color.Firebrick
+            Label2.BackColor = Color.Firebrick
+            Label3.BackColor = Color.Firebrick
+            Label4.BackColor = Color.Firebrick
+            Label5.BackColor = Color.Firebrick
+            Label6.BackColor = Color.LightBlue
+            Label7.BackColor = Color.LightBlue
+            Label8.BackColor = Color.LightBlue
+            Label9.BackColor = Color.LightBlue
+            Label10.BackColor = Color.Firebrick
+            Label11.BackColor = Color.Firebrick
+            Label12.BackColor = Color.Firebrick
+            Label13.BackColor = Color.Firebrick
+            Label14.BackColor = Color.Firebrick
+            Label15.BackColor = Color.Firebrick
+            Label16.BackColor = Color.Firebrick
+            Label17.BackColor = Color.LightBlue
+            Label18.BackColor = Color.LightBlue
+            Label19.BackColor = Color.LightBlue
+            Label20.BackColor = Color.LightBlue
+            Label21.BackColor = Color.Firebrick
+            Label22.BackColor = Color.Firebrick
+            Label23.BackColor = Color.Firebrick
+            Label24.BackColor = Color.Firebrick
+            Label25.BackColor = Color.Firebrick
+
+        ElseIf TimerLabel.Text = 3 Then
+            Label1.BackColor = Color.Firebrick
+            Label2.BackColor = Color.Firebrick
+            Label3.BackColor = Color.Firebrick
+            Label4.BackColor = Color.Firebrick
+            Label5.BackColor = Color.Firebrick
+            Label6.BackColor = Color.LightBlue
+            Label7.BackColor = Color.LightBlue
+            Label8.BackColor = Color.LightBlue
+            Label9.BackColor = Color.LightBlue
+            Label10.BackColor = Color.Firebrick
+            Label11.BackColor = Color.LightBlue
+            Label12.BackColor = Color.LightBlue
+            Label13.BackColor = Color.Firebrick
+            Label14.BackColor = Color.Firebrick
+            Label15.BackColor = Color.Firebrick
+            Label16.BackColor = Color.LightBlue
+            Label17.BackColor = Color.LightBlue
+            Label18.BackColor = Color.LightBlue
+            Label19.BackColor = Color.LightBlue
+            Label20.BackColor = Color.Firebrick
+            Label21.BackColor = Color.Firebrick
+            Label22.BackColor = Color.Firebrick
+            Label23.BackColor = Color.Firebrick
+            Label24.BackColor = Color.Firebrick
+            Label25.BackColor = Color.Firebrick
+
+        ElseIf TimerLabel.Text = 4 Then
+            Label1.BackColor = Color.Firebrick
+            Label2.BackColor = Color.LightBlue
+            Label3.BackColor = Color.LightBlue
+            Label4.BackColor = Color.LightBlue
+            Label5.BackColor = Color.Firebrick
+            Label6.BackColor = Color.Firebrick
+            Label7.BackColor = Color.LightBlue
+            Label8.BackColor = Color.LightBlue
+            Label9.BackColor = Color.LightBlue
+            Label10.BackColor = Color.Firebrick
+            Label11.BackColor = Color.Firebrick
+            Label12.BackColor = Color.Firebrick
+            Label13.BackColor = Color.Firebrick
+            Label14.BackColor = Color.Firebrick
+            Label15.BackColor = Color.Firebrick
+            Label16.BackColor = Color.LightBlue
+            Label17.BackColor = Color.LightBlue
+            Label18.BackColor = Color.LightBlue
+            Label19.BackColor = Color.LightBlue
+            Label20.BackColor = Color.Firebrick
+            Label21.BackColor = Color.LightBlue
+            Label22.BackColor = Color.LightBlue
+            Label23.BackColor = Color.LightBlue
+            Label24.BackColor = Color.LightBlue
+            Label25.BackColor = Color.Firebrick
+
+        ElseIf TimerLabel.Text = 5 Then
+            Label1.BackColor = Color.Firebrick
+            Label2.BackColor = Color.Firebrick
+            Label3.BackColor = Color.Firebrick
+            Label4.BackColor = Color.Firebrick
+            Label5.BackColor = Color.Firebrick
+            Label6.BackColor = Color.Firebrick
+            Label7.BackColor = Color.LightBlue
+            Label8.BackColor = Color.LightBlue
+            Label9.BackColor = Color.LightBlue
+            Label10.BackColor = Color.LightBlue
+            Label11.BackColor = Color.Firebrick
+            Label12.BackColor = Color.Firebrick
+            Label13.BackColor = Color.Firebrick
+            Label14.BackColor = Color.Firebrick
+            Label15.BackColor = Color.Firebrick
+            Label16.BackColor = Color.LightBlue
+            Label17.BackColor = Color.LightBlue
+            Label18.BackColor = Color.LightBlue
+            Label19.BackColor = Color.LightBlue
+            Label20.BackColor = Color.Firebrick
+            Label21.BackColor = Color.Firebrick
+            Label22.BackColor = Color.Firebrick
+            Label23.BackColor = Color.Firebrick
+            Label24.BackColor = Color.Firebrick
+            Label25.BackColor = Color.Firebrick
+
+        ElseIf TimerLabel.Text = 6 Then
+            Label1.BackColor = Color.Firebrick
+            Label2.BackColor = Color.Firebrick
+            Label3.BackColor = Color.Firebrick
+            Label4.BackColor = Color.Firebrick
+            Label5.BackColor = Color.Firebrick
+            Label6.BackColor = Color.Firebrick
+            Label7.BackColor = Color.LightBlue
+            Label8.BackColor = Color.LightBlue
+            Label9.BackColor = Color.LightBlue
+            Label10.BackColor = Color.LightBlue
+            Label11.BackColor = Color.Firebrick
+            Label12.BackColor = Color.Firebrick
+            Label13.BackColor = Color.Firebrick
+            Label14.BackColor = Color.Firebrick
+            Label15.BackColor = Color.Firebrick
+            Label16.BackColor = Color.Firebrick
+            Label17.BackColor = Color.LightBlue
+            Label18.BackColor = Color.LightBlue
+            Label19.BackColor = Color.LightBlue
+            Label20.BackColor = Color.Firebrick
+            Label21.BackColor = Color.Firebrick
+            Label22.BackColor = Color.Firebrick
+            Label23.BackColor = Color.Firebrick
+            Label24.BackColor = Color.Firebrick
+            Label25.BackColor = Color.Firebrick
+
+        ElseIf TimerLabel.Text = 7 Then
+            Label1.BackColor = Color.Firebrick
+            Label2.BackColor = Color.Firebrick
+            Label3.BackColor = Color.Firebrick
+            Label4.BackColor = Color.Firebrick
+            Label5.BackColor = Color.Firebrick
+            Label6.BackColor = Color.LightBlue
+            Label7.BackColor = Color.LightBlue
+            Label8.BackColor = Color.LightBlue
+            Label9.BackColor = Color.Firebrick
+            Label10.BackColor = Color.LightBlue
+            Label11.BackColor = Color.LightBlue
+            Label12.BackColor = Color.LightBlue
+            Label13.BackColor = Color.Firebrick
+            Label14.BackColor = Color.LightBlue
+            Label15.BackColor = Color.LightBlue
+            Label16.BackColor = Color.LightBlue
+            Label17.BackColor = Color.LightBlue
+            Label18.BackColor = Color.Firebrick
+            Label19.BackColor = Color.LightBlue
+            Label20.BackColor = Color.LightBlue
+            Label21.BackColor = Color.LightBlue
+            Label22.BackColor = Color.LightBlue
+            Label23.BackColor = Color.Firebrick
+            Label24.BackColor = Color.LightBlue
+            Label25.BackColor = Color.LightBlue
+
+        ElseIf TimerLabel.Text = 8 Then
+            Label1.BackColor = Color.Firebrick
+            Label2.BackColor = Color.Firebrick
+            Label3.BackColor = Color.Firebrick
+            Label4.BackColor = Color.Firebrick
+            Label5.BackColor = Color.Firebrick
+            Label6.BackColor = Color.Firebrick
+            Label7.BackColor = Color.LightBlue
+            Label8.BackColor = Color.LightBlue
+            Label9.BackColor = Color.LightBlue
+            Label10.BackColor = Color.Firebrick
+            Label11.BackColor = Color.Firebrick
+            Label12.BackColor = Color.Firebrick
+            Label13.BackColor = Color.Firebrick
+            Label14.BackColor = Color.Firebrick
+            Label15.BackColor = Color.Firebrick
+            Label16.BackColor = Color.Firebrick
+            Label17.BackColor = Color.LightBlue
+            Label18.BackColor = Color.LightBlue
+            Label19.BackColor = Color.LightBlue
+            Label20.BackColor = Color.Firebrick
+            Label21.BackColor = Color.Firebrick
+            Label22.BackColor = Color.Firebrick
+            Label23.BackColor = Color.Firebrick
+            Label24.BackColor = Color.Firebrick
+            Label25.BackColor = Color.Firebrick
+
+        ElseIf TimerLabel.Text = 9 Then
+            Label1.BackColor = Color.Firebrick
+            Label2.BackColor = Color.Firebrick
+            Label3.BackColor = Color.Firebrick
+            Label4.BackColor = Color.Firebrick
+            Label5.BackColor = Color.Firebrick
+            Label6.BackColor = Color.Firebrick
+            Label7.BackColor = Color.LightBlue
+            Label8.BackColor = Color.LightBlue
+            Label9.BackColor = Color.LightBlue
+            Label10.BackColor = Color.Firebrick
+            Label11.BackColor = Color.Firebrick
+            Label12.BackColor = Color.Firebrick
+            Label13.BackColor = Color.Firebrick
+            Label14.BackColor = Color.Firebrick
+            Label15.BackColor = Color.Firebrick
+            Label16.BackColor = Color.LightBlue
+            Label17.BackColor = Color.LightBlue
+            Label18.BackColor = Color.LightBlue
+            Label19.BackColor = Color.LightBlue
+            Label20.BackColor = Color.Firebrick
+            Label21.BackColor = Color.Firebrick
+            Label22.BackColor = Color.Firebrick
+            Label23.BackColor = Color.Firebrick
+            Label24.BackColor = Color.Firebrick
+            Label25.BackColor = Color.Firebrick
+
+        Else
+            TimerLabel.Text = 0
+
+            Label1.BackColor = Color.LightBlue
+            Label2.BackColor = Color.Firebrick
+            Label3.BackColor = Color.Firebrick
+            Label4.BackColor = Color.Firebrick
+            Label5.BackColor = Color.LightBlue
+            Label6.BackColor = Color.Firebrick
+            Label7.BackColor = Color.LightBlue
+            Label8.BackColor = Color.LightBlue
+            Label9.BackColor = Color.LightBlue
+            Label10.BackColor = Color.Firebrick
+            Label11.BackColor = Color.Firebrick
+            Label12.BackColor = Color.LightBlue
+            Label13.BackColor = Color.LightBlue
+            Label14.BackColor = Color.LightBlue
+            Label15.BackColor = Color.Firebrick
+            Label16.BackColor = Color.Firebrick
+            Label17.BackColor = Color.LightBlue
+            Label18.BackColor = Color.LightBlue
+            Label19.BackColor = Color.LightBlue
+            Label20.BackColor = Color.Firebrick
+            Label21.BackColor = Color.LightBlue
+            Label22.BackColor = Color.Firebrick
+            Label23.BackColor = Color.Firebrick
+            Label24.BackColor = Color.Firebrick
+            Label25.BackColor = Color.LightBlue
+
+        End If
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Timer1.Stop()
+
+    End Sub
 End Class
